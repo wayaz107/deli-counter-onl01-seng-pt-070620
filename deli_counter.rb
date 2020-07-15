9b2e65218b508ce0d1049bd2ed7c8c32a509514b
Warda Ayaz <wayaz107@gmail.com>
2020-07-15 01:20:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,16 @@
 # Write your code here.
 
-def line(place)
-  if place.empty?
-       "The line is currently empty."
+def line(deli)
+  if deli.empty?
+      puts "The line is currently empty."
   else
-    "You are currently in line #{place}."
+   current_line = "The line is currently: "
+   deli.each.with_index(1) do |i, name|
+     current_line << "#{i}. #{name}"
+  end 
+  current_line
 end
-
+ 
 def take_a_number (katz_deli, name)
   katz_deli = []
   puts "Hello #{name}!, you are currently in line number #{index + 1}"
