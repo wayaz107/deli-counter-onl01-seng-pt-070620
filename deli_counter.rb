1556adbf464e2fccf06e95234ac810be09ee6439
Warda Ayaz <wayaz107@gmail.com>
2020-07-15 00:56:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,27 @@
 # Write your code here.
+
 def line(place)
-    return "The line is currently empty" if place.empty?
+  if place.empty?
+       "The line is currently empty." 
+  else
+    "You are currently in line #{place}."
 end
+
+def take_a_number (katz_deli, name) 
+  katz_deli = []
+  puts "Hello #{name}!, you are currently in line number #{index + 1}"
+end
+
+def now_serving(next_person)
+  if next_person.empty? 
+    puts "There is nobody waiting to be served!"
+  else
+  puts "The next person in line is #{next_person}"
+  next_person.shift
+end
+end
+
+
+  
 
   
